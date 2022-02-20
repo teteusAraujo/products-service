a2bc41fe90f679b086bb31108ffd067ea27d9032
Mateus Araujo <mateusaraujo996@gmail.com>
2022-02-20 14:32:43 -0300

    feat : criando metodo de busca por nome no Banco de dados

--- src/main/kotlin/com/mateus/repository/ProductRepository.kt
+++ src/main/kotlin/com/mateus/repository/ProductRepository.kt
@@ -6,4 +6,5 @@ import io.micronaut.data.jpa.repository.JpaRepository
 
 @Repository
 interface ProductRepository : JpaRepository<Product, Long> {
+    fun findByNameIgnoreCase(name: String): Product?
 }
